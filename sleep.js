b6aa7ec5611860a49c7c91b3d18fb434c8df82b5
Desarae <desaraegabrielle@icloud.com>
2019-04-29 22:31:10 -0700

    actualSleepHours calculated against idealSleepHours

---
+++ sleep.js
@@ -0,0 +1,23 @@
+// calculate the hours slept a 7 day  week period
+const getActualSleepHours = () => 6 + 7 + 9 + 8 + 5 + 10 + 11;
+
+// the ideal hours per night you want to sleep
+const getIdealSleepHours = () => {
+  const idealHours = 8;
+  return idealHours * 7;
+}
+// calculate the actual hours you slept against the ideal sleep hours
+const calculateSleepDebt = () => {
+  const actualSleepHours = getActualSleepHours();
+  const idealSleepHours = getIdealSleepHours();
+  if (actualSleepHours === idealSleepHours) {
+  console.log('you got the perfect amount of sleep, sis.');
+} else if (actualSleepHours > idealSleepHours) {
+  console.log('you got more sleep than needed!');
+} else {
+  console.log('you need more sleep, sis. you only got '  + (idealSleepHours - actualSleepHours) + ' hours sleep this week.');
+}
+  
+}
+// run the function of calculation of sleep
+calculateSleepDebt();
